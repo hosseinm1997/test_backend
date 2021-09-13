94ad21ad4eece43aedce3803d243e536ce601ed5
ghanbari <ghanbarimreza6@gmail.com>
2021-09-14 00:56:29 +0430

    feat: add helper function with sajaamax project

---
+++ app/helpers.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!function_exists('sendSmsByPattern')) {
+    function sendSmsByPattern($mobile, $parameters)
+    {
+//        $pattern_code = "thv37ghfb6";
+//        $to = array($mobile);
+//        $input_data = array("code" => $code);
+//        $url = "https://ippanel.com/patterns/pattern?username=" . $username . "&password=" . urlencode($password) . "&from=$from&to=" . json_encode($to) . "&input_data=" . urlencode(json_encode($input_data)) . "&pattern_code=$pattern_code";
+//        $handler = curl_init($url);
+//        curl_setopt($handler, CURLOPT_CUSTOMREQUEST, "POST");
+//        curl_setopt($handler, CURLOPT_POSTFIELDS, $input_data);
+//        curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
+//        return curl_exec($handler);
+    }
+}
