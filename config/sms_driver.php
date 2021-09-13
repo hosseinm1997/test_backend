6688e9a98553e06ad178153d735ca40c0ed8a754
ghanbari <ghanbarimreza6@gmail.com>
2021-09-14 01:19:38 +0430

    feat: add farazsms service with project.

--- config/sms_driver.php
+++ config/sms_driver.php
@@ -3,8 +3,14 @@ return [
     'farazsms' => [
         'username' => 'reseller09027818262',
         'password' => 'fartakvira2021@',
+        'address' => [
+            'pattern' =>"https://ippanel.com/patterns/pattern?username="
+        ],
         'numbers' => [
             'pattern' => "+983000505"
+        ],
+        'patterns' => [
+            'otp' => "thv37ghfb6"
         ]
         ]
     ];
---
+++ infrastructure/Service/FarazSms.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Infrastructure\Service;
+
+use App\Models\User;
+use App\Http\Requests\RegisterRequest;
+
+class FarazSms
+{
+    private $config;
+
+    public function __construct()
+    {
+        $this->config = config('sms_driver.farazsms');
+    }
+
+    public function sendSmsByPattern($mobile, $parameters)
+    {
+        $pattern_code = $this->config['patterns']['otp'];
+        $to = array($mobile);
+        $input_data = $parameters;
+        $from = $this->config['numbers']['pattern'];
+
+        $url = $this->config['address']['pattern']
+            . $this->config['username']
+            ."&password=" .urlencode($this->config['password'])
+            ."&from=$from&to=" . json_encode($to)
+            . "&input_data=" .
+            urlencode(json_encode($input_data))
+            . "&pattern_code=$pattern_code";
+        $handler = curl_init($url);
+        curl_setopt($handler, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($handler, CURLOPT_POSTFIELDS, $input_data);
+        curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
+
+        return curl_exec($handler);
+    }
+
+}
