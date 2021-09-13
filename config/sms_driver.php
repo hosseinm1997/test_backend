d9e994d945a7be2a0b8c5146084288d051ae4e56
ghanbari <ghanbarimreza6@gmail.com>
2021-09-13 23:32:59 +0430

    feat: add farazsms driver

---
+++ config/sms_driver.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'farazsms' => [
+        'username' => 'reseller09027818262',
+        'password' => 'fartakvira2021@',
+        'numbers' => [
+            'pattern' => "+983000505"
+        ]
+        ]
+    ];
