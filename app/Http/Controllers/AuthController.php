15abbafa8a6bedcab2837ce34f0d0f16c5781aef
ghanbari <ghanbarimreza6@gmail.com>
2021-09-17 22:24:03 +0430

    feat: add check verification code function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,9 +8,12 @@ use App\Models\VerificationCode;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Carbon\Carbon;
+use Laravel\Sanctum\HasApiTokens;
 
 class AuthController extends Controller
 {
+    use HasApiTokens;
+
     public function checkUserForRegister(RegisterRequest $request)
     {
         $user = $this->findUserByMobileOrNationalCode($request);
@@ -46,7 +49,7 @@ class AuthController extends Controller
 
     private function registerUser($request)
     {
-        User::createOrFail(['mobile' => $request->mobile, 'national_code' => $request->nationalCode]);
+        User::create(['mobile' => $request->mobile, 'national_code' => $request->nationalCode]);
     }
 
     function findUserByMobileOrNationalCode($request)
@@ -76,12 +79,12 @@ class AuthController extends Controller
         ]);
     }
 
-    public function preparingToSendSms($request, $otp)
+    public function preparingToSendSms($mobile, $otp)
     {
         $smsService = new FarazSms();
 
         return $smsService->sendSmsByPattern(
-            $request->mobile, array('code' => $otp)
+            $mobile, array('code' => $otp)
         );
     }
 
@@ -91,7 +94,7 @@ class AuthController extends Controller
         $this->saveVerificationCode($mobile, $otp);
         $result = $this->preparingToSendSms($mobile, $otp);
 
-     //   todo: After launch sentry or log change the report
+        //   todo: After launch sentry or log change the report
         if (!is_numeric($result)) {
             abort(500, 'خطا: سامانه پیامکی دچار اختلال شده است لطفا بعدا تلاش کنید.');
         }
@@ -105,12 +108,12 @@ class AuthController extends Controller
         $timeOutSms = Carbon::now()->subMinutes(config('auth.verification_sms_timeout'));
 
         $request->validate([
-            'mobile' => 'required|min:11|not_regex:"/^09[0-9]{9}$/"|exists:users,mobile'
+            'mobile' => 'required|min:11|not_regex:"/^09[0-9]{9}$/"|exists:users,mobile|max:11'
         ]);
 
         $verification = VerificationCode::where('mobile', $request->mobile)
             ->where('used_at', null)
-            ->where('updated_at', '>', $timeOutSms)
+            ->where('created_at', '>', $timeOutSms)
             ->orderBy('id', 'DESC')
             ->first();
 
@@ -137,8 +140,48 @@ class AuthController extends Controller
         }
     }
 
-    public function checkVerificationCode()
+    public function checkVerificationCode(Request $request)
     {
+        $request->validate([
+            'code' => 'required|max:5|min:5',
+            'mobile' => 'required|min:11|not_regex:"/^09[0-9]{9}$/"|exists:users,mobile|max:11'
+        ]);
 
+        $timeOutSms = Carbon::now()->subMinutes(config('auth.verification_sms_timeout'));
+
+        $verification = VerificationCode::where('mobile', $request->mobile)
+            ->where('used_at', null)
+            ->where('code', $request->code)
+            ->where('mobile', $request->mobile)
+            ->where('created_at', '>', $timeOutSms)
+            ->first();
+
+        if (is_null($verification)) {
+            abort('422', 'کد نامعتبر');
+        }
+
+        $this->updateUsedAtVerificationCode($verification->id);
+
+        $user = User::where('mobile', $request->mobile)->first();
+
+        if (is_null($user)) {
+            abort('500');
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        if (is_null($token)) {
+            abort('500');
+        } else {
+            // todo: after lunch faraz sms create sms for successfully register.
+            return ["message" => "ثبت نام شما با موفقیت تکمیل شد", 'result' => true];
+        }
+    }
+
+    private function updateUsedAtVerificationCode($verificationId)
+    {
+        VerificationCode::where('id', $verificationId)->update([
+            'used_at' => Carbon::now()
+        ]);
     }
 }
