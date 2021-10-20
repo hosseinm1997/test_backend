e1bb4f92e8af1fc5d96b5809959addde1abc7f8a
mshahebrahimi77 <mshahebrahimi77@gmail.com>
2021-10-21 02:58:39 +0330

    feat:add PriorityEnums

---
+++ app/Enumerations/PriorityEnums.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Enumerations;
+
+class PriorityEnums
+{
+    const GROUP_ID = 50;
+
+    const PRIORITY_LOW = 51;
+    const PRIORITY_NORMAL = 52;
+    const PRIORITY_EMERGENCY = 53;
+    const PRIORITY_HIGH = 54;
+
+
+    public static function getEnumPriority(): array
+    {
+        return [
+            self::PRIORITY_LOW,
+            self::PRIORITY_HIGH,
+            self::PRIORITY_NORMAL,
+            self::PRIORITY_EMERGENCY,
+        ];
+    }
+}
