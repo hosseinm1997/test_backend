4201277ac8ecce947cfb096657ad33b470ccee8d
mshahebrahimi77 <mshahebrahimi77@gmail.com>
2021-10-21 23:29:59 +0330

    fix:implement helper generateAddress and uploadFile

--- app/helpers.php
+++ app/helpers.php
@@ -1,6 +1,10 @@
 <?php
 
+use Illuminate\Http\UploadedFile;
+use Vinkla\Hashids\Facades\Hashids;
+use App\Repositories\FileRepository;
 use Infrastructure\Service\FarazSms;
+use App\Enumerations\FileCategoryEnums;
 
 if (!function_exists('sendSmsByPattern')) {
 
@@ -47,5 +51,36 @@ if (!function_exists('storage_app_path')) {
     function storage_app_path($path): string
     {
         return storage_path('app' . DIRECTORY_SEPARATOR . $path);
+    }
+}
+
+if (!function_exists('generateAddress')) {
+
+    function generateAddress(UploadedFile $file, string $directory, int $entityId)
+    {
+        try {
+            $directory = $directory . DIRECTORY_SEPARATOR . Hashids::encode($entityId);
+
+            return $file->store($directory);
+        } catch (\Throwable $exception) {
+            throw new \Exception('امکان ارسال فایل وجود ندارد!');
+        }
+    }
+}
+
+if (!function_exists('uploadFile')) {
+
+    /**
+     * @throws Exception
+     */
+    function uploadFile(UploadedFile $file, string $directory, int $entityId): array
+    {
+        $repo = new FileRepository();
+
+        return $repo->create([
+            'address' => generateAddress($file, $directory, $entityId),
+            'contentType' => $file->getType(),
+            'category' => FileCategoryEnums::DOCUMENT,
+        ]);
     }
 }
