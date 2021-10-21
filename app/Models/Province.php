192f4c0ec127e43cda10dd3c722878d1e1d3646e
mshahebrahimi77 <mshahebrahimi77@gmail.com>
2021-10-21 20:21:50 +0330

    feat:use traits searchable and sortable in model Province

--- app/Models/Province.php
+++ app/Models/Province.php
@@ -3,12 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Infrastructure\Traits\Sortable\SortableTrait;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Infrastructure\Traits\Searchable\SearchableTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Province extends Model
 {
-    use HasFactory;
+    use HasFactory, SearchableTrait, SortableTrait;
 
     protected $table = 'provinces';
+
     protected $hidden = ['created_at'];
+
+    public $searchable = ['id', 'title'];
+
+    public $sortable = ['id'];
+
+    public function cities(): HasMany
+    {
+        return $this->hasMany(City::class, 'province_id');
+    }
 }
