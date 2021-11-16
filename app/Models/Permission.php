e02568af9a1a6648d1006d3e767bd2f5a8113325
mshahebrahimi77 <mshahebrahimi77@gmail.com>
2021-11-16 22:46:11 +0330

    fix:change name

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -3,9 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Spatie\Permission\Models\Permission as permissionSpatie;
+use Spatie\Permission\Models\Permission as PermissionSpatie;
 
-class Permission extends permissionSpatie
+class Permission extends PermissionSpatie
 {
     use HasFactory;
 }
